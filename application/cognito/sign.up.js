e6aa006fa0b5ba7783bdab9b8d8f4ecbaa40f9f5
kanamaru_shuya <shellkana@gmail.com>
2018-05-14 22:42:05 +0900

    コード認証の追加

--- application/cognito/sign.up.js
+++ application/cognito/sign.up.js
@@ -1,2 +1,21 @@
 var json = JSON;
 console.log(json);
+var ConfirmView = function () {
+    ViewBase.call(this);
+    this.Title = "Index";
+    this.Name = "ConfirmView";
+};
+inherits(ConfirmView, ViewBase);
+ConfirmView.prototype.StartView = function () {
+    ViewBase.prototype.StartView.call(this);
+    var _this = this;
+    var uri = new URI();
+    var code = this.AppendInput("認証コード", "xxxxxx", "メアドに届いたコードを入力や！");
+    this.AppendButton("認証する", function () {
+        apiManager.ExecApi("cognito/Confirm", {
+            Name: tempData.cognitoUserName,
+            Password: code.val()
+        })
+    });
+};
+viewManager.ChangeView(ConfirmView);
