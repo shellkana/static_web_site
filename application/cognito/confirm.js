f2824181a7d55f2e813f95d98459054545039ae5
kanamaru_shuya <shellkana@gmail.com>
2018-05-14 23:39:06 +0900

    認証後の遷移

--- application/cognito/confirm.js
+++ application/cognito/confirm.js
@@ -3,7 +3,7 @@ console.log(json);
 modalManager.OpenModal(function (_modal) {
     _modal.SetTitle("認証成功");
     _modal.AppendText("認証に成功したくさいで");
-    _modal.bind('hide', function () {
+    _modal.on("hidden.bs.modal", function () {
         viewManager.ChangeView(IndexView);
     });
 });
